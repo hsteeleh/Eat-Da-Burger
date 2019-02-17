101fa62bc66f99708554332684c6ba69f6f9774f
Hannah <37351110+hsteeleh@users.noreply.github.com>
2019-02-17 17:11:54 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,33 @@
 //setup the code to connect Node to MySQL.
+
+// Pull dependencies
+var mysql = require('mysql');
+
+// MySQL connection object
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'Viracocina_db'
+	})
+};
+
+// Connection to MySQL
+connection.connect(function(err) {
+  if (err) {
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+});
+
+// Export connection for ORM use
+module.exports = connection;
