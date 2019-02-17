2b5534d7a22b64c8e3f63cefab4fd90a4352146e
Hannah <37351110+hsteeleh@users.noreply.github.com>
2019-02-17 17:25:00 -0500

    Update viracocina.js

--- config/modules/viracocina.js
+++ config/modules/viracocina.js
@@ -2,3 +2,33 @@
 
 // Also inside viracocina.js, create the code that will call the ORM functions using viracocina specific input for the ORM.
 // Export at the end of the viracocina.js file.
+
+// Import the ORM to implement functions that will interact with the database
+var orm = require('../config/orm.js');
+
+// Create the comida object
+var comida = {
+  // Select all comida table entries
+  selectAll: function(cb) {
+    orm.selectAll('comidas', function(res) {
+      cb(res);
+    });
+  },
+
+  // The variables cols and vals are arrays
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne('comidas', cols, vals, function(res) {
+      cb(res);
+    });
+  },
+
+  // The objColVals is an object specifying columns as object keys with associated values
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne('comidas', objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (comidaController.js).
+module.exports = comida;
