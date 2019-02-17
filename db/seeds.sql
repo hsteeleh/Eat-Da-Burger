5ee009ed30916313947e658ba2af4d40f4732c1f
Hannah <37351110+hsteeleh@users.noreply.github.com>
2019-02-17 17:22:09 -0500

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,8 @@
 -- Write insert queries to populate the burgers table with at least three entries.
+-- Creating seed data for the Viracocina_db database --
+INSERT INTO Viracocina (comida_name, devoured) VALUES ('Lomo Saltado', false);
+INSERT INTO Viracocina (comida_name, devoured) VALUES ('Hamberguesa de Alpacha', false);
+INSERT INTO Viracocina (comida_name, devoured) VALUES ('Aji de Gallina', false);
+INSERT INTO Viracocina (comida_name, devoured) VALUES ('Ceviche', true);
+INSERT INTO Viracocina (comida_name, devoured) VALUES ('Papas a la Huanciana', true);
+INSERT INTO Viracocina (comida_name, devoured) VALUES ('Cuy', true);
